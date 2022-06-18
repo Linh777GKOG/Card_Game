a1e1374afefefbed48d43289819b6f0cc715c4bc
Linh Vu <linh777gkog@gmail.com>
2022-06-18 22:13:00 +0700

    push card

--- deck.js
+++ deck.js
@@ -14,3 +14,30 @@ const VALUES = [
   'Q',
   'K',
 ];
+
+export default class Deck {
+  constructor(cards = freshDeck()) {
+    this.cards = cards;
+  }
+
+  get numberOfCards() {
+    return this.cards.length;
+  }
+
+  pop() {
+    return this.cards.shift();
+  }
+
+  push(card) {
+    this.cards.push(card);
+  }
+
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1));
+      const oldValue = this.cards[newIndex];
+      this.cards[newIndex] = this.cards[i];
+      this.cards[i] = oldValue;
+    }
+  }
+}
